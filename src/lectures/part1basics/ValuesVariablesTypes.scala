f01f5761f17cea9b84a0406494067e099bac4aaa
Felipe Lima <flf.felipe@gmail.com>
2020-09-13 20:03:08 -0300

    Variaveis, tipos e Imutabilidade

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,25 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  //  val x: Int = 42
+  val x = 42
+  println(x)
+
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShor: Short = 4563
+  val aLong: Long = 515151515578791318L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects
+
+}
