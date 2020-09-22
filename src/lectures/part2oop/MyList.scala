8f944b33e884a1767f9cc1888ae96c59ce448c43
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2020-09-22 13:04:37 -0300

    Inheritance exercises - final part

--- src/lectures/part2oop/MyList.scala
+++ src/lectures/part2oop/MyList.scala
@@ -13,7 +13,8 @@ abstract class MyList {
   def tail: MyList
   def isEmpty: Boolean
   def add(item: Int): MyList
-//  def toString(): String
+  def printElements: String
+  override def toString: String = "[" + printElements + "]"
 }
 
 object EmptyList extends MyList {
@@ -21,6 +22,7 @@ object EmptyList extends MyList {
   def tail: MyList = throw new NoSuchElementException
   def isEmpty: Boolean = true
   def add(item: Int): MyList = new Cons(item, EmptyList)
+  def printElements: String = ""
 }
 
 class Cons(h: Int, t: MyList) extends MyList {
@@ -28,10 +30,15 @@ class Cons(h: Int, t: MyList) extends MyList {
   def tail: MyList = t
   def isEmpty: Boolean = false
   def add(item: Int): MyList = new Cons(item, this)
+  def printElements: String =
+    if (t.isEmpty) "" + h
+    else h + " " + t.printElements
 }
 
 object ListTest extends App {
   val list = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
   println(list.tail.head)
   println(list.add(4).head)
+  println(list.isEmpty)
+  println(list.toString)
 }
